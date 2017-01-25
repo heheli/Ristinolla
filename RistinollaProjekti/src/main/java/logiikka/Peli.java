8030955bd98a243ee1cd2022703a8b1f6947e1fe
Poutiainen Heli V A <poutihel@da5-cs-bk107-05.helsinki.fi>
2017-01-25 13:20:26 +0200

    Luokkien muokkausta

--- RistinollaProjekti/src/main/java/logiikka/Peli.java
+++ RistinollaProjekti/src/main/java/logiikka/Peli.java
@@ -2,31 +2,23 @@
 package logiikka;
 
 import kayttaja.Pelaaja;
-import kayttaja.PelaajaVarasto;
 
 
-public class Ristinolla {
+public class Peli {
     
     private int pelinKoko;
     private Pelaaja pelaaja1;
     private Pelaaja pelaaja2;
-    private PelaajaVarasto varasto;
     
-    public Ristinolla(int koko){ //esimerkiksi kolme kokoa; 3x3, 6x6 ja 9x9
+    public Peli(int koko){ //esimerkiksi kolme kokoa; 3x3, 6x6 ja 9x9
         this.pelinKoko = koko;
         this.pelaaja1 = null;
         this.pelaaja2 = null;
-        this.varasto = new PelaajaVarasto();
     }
     
     public void setPelaajat(Pelaaja eka, Pelaaja toka){
         this.pelaaja1 = eka;
         this.pelaaja2 = toka;
-        this.varasto.lisaaPelaaja(this.pelaaja1);
-        this.varasto.lisaaPelaaja(this.pelaaja2);
-    }
-    public PelaajaVarasto getVarasto(){
-        return varasto;
     }
     
     public void kaynnista(){
--- RistinollaProjekti/src/test/java/ristinolla/logiikka/RistinollaTest.java
+++ RistinollaProjekti/src/test/java/ristinolla/logiikka/RistinollaTest.java
@@ -1,31 +1,22 @@
 package ristinolla.logiikka;
 
 
-
 import kayttaja.Pelaaja;
-import kayttaja.PelaajaVarasto;
-import logiikka.Ristinolla;
-import logiikka.Ristinolla;
-import org.junit.After;
-import org.junit.AfterClass;
+import logiikka.Peli;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+
 
 
 public class RistinollaTest {
     
     Pelaaja eka;
     Pelaaja toka;
-    PelaajaVarasto varasto;
-    Ristinolla uusiPeli;
+    Peli uusiPeli;
     
     public RistinollaTest() {
         eka = new Pelaaja();
         toka = new Pelaaja();
-        varasto = new PelaajaVarasto();
-        uusiPeli = new Ristinolla(1);
+        uusiPeli = new Peli(1);
     }
     
     @Before
@@ -35,10 +26,6 @@ public class RistinollaTest {
         uusiPeli.setPelaajat(eka, toka);
     }
     
-    @Test 
-    public void alkuTilanne(){
-        assertEquals(2, uusiPeli.getVarasto().getPelaajaLista().size());
-    }
     
 
 }
